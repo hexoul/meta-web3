069777da7b9c3c623a170079aacd25183be20e74
hexoul <crosien@gmail.com>
2019-03-21 17:46:03 +0900

    fix: wrong testunit

--- example/src/App.js
+++ example/src/App.js
@@ -27,54 +27,51 @@ export default class App extends Component {
         node: '0x6f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0',
         ip: '0x31302e332e35382e36',
         port: 30303,
-        amount: '1',
+        amount: '1'
       }
       // All contracts are initialized
       this.result = {
         // getLengthOfAchievements: await contracts.achievementManager.getLengthOfAchievements(),
         // getAttestationAgencyNum: await contracts.aaRegistry.getAttestationAgencyNum(),
         // getTotal: await contracts.topicRegistry.getTotal()\
-        getBallotBasic_creator: (await contracts.ballotStorage.getBallotBasic(1)).creator,
-        getBallotMember_newMemberAddress: (await contracts.ballotStorage.getBallotMember(1)).newMemberAddress,
+        getBallotBasicCreator: (await contracts.ballotStorage.getBallotBasic(1)).creator,
+        getBallotMemberNewMemberAddress: (await contracts.ballotStorage.getBallotMember(1)).newMemberAddress,
         getMinVotingDuration: await contracts.ballotStorage.getMinVotingDuration(),
         getMaxVotingDuration: await contracts.ballotStorage.getMaxVotingDuration(),
-        updateBallotDuration_to: (await contracts.ballotStorage.updateBallotDuration(1, 86400)).to,
-        updateBallotMemo_to: (await contracts.ballotStorage.updateBallotMemo(1, '0x54657374')).to,
-        cancelBallot_to: (await contracts.ballotStorage.cancelBallot(1)).to,
-
+        updateBallotDurationData: (await contracts.ballotStorage.updateBallotDuration(1, 86400)).data,
+        updateBallotMemoData: (await contracts.ballotStorage.updateBallotMemo(1, '0x54657374')).data,
+        cancelBallotData: (await contracts.ballotStorage.cancelBallot(1)).data,
         getStakingMin: await contracts.envStorage.getStakingMin(),
         getStakingMax: await contracts.envStorage.getStakingMax(),
-
         getBallotLength: await contracts.governance.getBallotLength(),
         isMember: await contracts.governance.isMember('0xC03B19F95D409c26b64B44292827a26989D2E8d0'),
-        vote_to: (await contracts.governance.vote(1, true)).to,
-        addProposalToAddMember_to: (await contracts.governance.addProposalToAddMember(
+        voteData: (await contracts.governance.vote(1, true)).to,
+        addProposalToAddMemberData: (await contracts.governance.addProposalToAddMember(
           testProposal.addr,
           testProposal.textToHex,
           testProposal.node,
           testProposal.ip,
           [testProposal.port, testProposal.amount],
           testProposal.textToHex
-        )).to,
-        addProposalToChangeMember_to: (await contracts.governance.addProposalToChangeMember(
+        )).data,
+        addProposalToChangeMemberData: (await contracts.governance.addProposalToChangeMember(
           [testProposal.addr, testProposal.addr],
           testProposal.textToHex,
           testProposal.node,
           testProposal.ip,
           [testProposal.port, testProposal.amount],
           testProposal.textToHex
-        )).to,
-        addProposalToRemoveMember_to: (await contracts.governance.addProposalToRemoveMember(
+        )).data,
+        addProposalToRemoveMemberData: (await contracts.governance.addProposalToRemoveMember(
           testProposal.addr,
           testProposal.amount,
           testProposal.textToHex
-        )).to,
-
+        )).data,
         lockedBalanceOf: await contracts.staking.lockedBalanceOf('0xC03B19F95D409c26b64B44292827a26989D2E8d0'),
         balanceOf: await contracts.staking.balanceOf('0xC03B19F95D409c26b64B44292827a26989D2E8d0'),
         availableBalanceOf: await contracts.staking.availableBalanceOf('0xC03B19F95D409c26b64B44292827a26989D2E8d0'),
-        deposit_to: (await contracts.staking.deposit()).to,
-        withdraw_to: (await contracts.staking.withdraw('1')).to
+        depositData: (await contracts.staking.deposit()).data,
+        withdrawData: (await contracts.staking.withdraw('1')).data
       }
       this.setState({ initDone: true })
     })
--- src/components/BallotStorage.contract.js
+++ src/components/BallotStorage.contract.js
@@ -71,4 +71,5 @@ class BallotStorage {
     }
   }
 }
+
 export { BallotStorage }
